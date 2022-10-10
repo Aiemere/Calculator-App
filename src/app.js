59feb4bc3e5e88c7fe72feed064ff002184b88ba
Laura Asije <asijebaby@gmail.com>
2022-10-10 22:39:05 +0200

    Fix the math symbols
    Fix the math symbols so it is possible to use different signs in one expression, add or minus or times
    or divide numbers in units, tens, hundreds, and so on. Also used indexes of results of expressions
    to track the answers and use them when you are deleting some numbers from the expression.

--- src/app.js
+++ src/app.js
@@ -37,55 +37,176 @@ if(x2.length > 11){
     calcScreen.style.overflowX = "scroll"
 }
 let valuesArray = []
+let indexArray = []
 numbers.forEach(number => number.addEventListener("click",() => {
     setTimeout(() => {
         let value = expression.value
-        console.log(value)
+        let expx = value.split("+").join(",").split("−").join(",").split("×").
+        join(",").split("÷").join(",").split(",")
         let exp1 = value.split("")
+        //let expx1 = Array.from(expx)
+        //let expx2 = expx1.map(num => num * 1)
         if(exp1.includes("+") == true){
+            //let indexArray = []
             let exp2 = value.split("+")
             let from = Array.from(exp2)
             let from2 = from.pop()
             let exp3 = from2 * 1
+            let index = expx.indexOf(from2,-1)
+            indexArray.push(index)
+            indexArray.forEach(num => {
+                if(num == -1) {
+                let z = indexArray.indexOf(num)
+                indexArray.splice(z,1)
+                }
+            })
+            let indexArraySlice = indexArray.slice(-2)
+            console.log(expx)
+            console.log(indexArray)
+            console.log(indexArraySlice)
             if(isNaN(exp3) == false){
-                let exp4 = valuesArray[0] + exp3
-                valuesArray.splice(0,1, exp4)
-                console.log(valuesArray)
+                if(index == indexArraySlice[0]){
+                    valuesArray.pop()
+                    indexArray.pop()
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 + exp3
+                    valuesArray.push(exp4)
+                    console.log(indexArray)
+                    console.log(valuesArray)
+                    
+                }else if (index != indexArraySlice[0]){
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 + exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                    console.log(index)
+                    console.log(expx)
+                }
+                
             }else if(isNaN(exp3) == true){
                 console.log("exp3 is NaN")
             }
-            
-            
+            console.log(valuesArray)
+            //let from3 = from2.reduce((pv, cv) => pv - cv)
+            //console.log(from3)
+            //valuesArray.splice(0,1, exp4)
+            //let from2 = from.map(num => num * 1)
         }
-        if(exp1.includes("−")){
+        if(exp1.includes("−") == true){
+            //let indexArray = []
             let exp2 = value.split("−")
             let from = Array.from(exp2)
             let from2 = from.pop()
             let exp3 = from2 * 1
+            let index = expx.indexOf(from2,-1)
+            indexArray.push(index)
+            indexArray.forEach(num => {
+                if(num == -1) {
+                let z = indexArray.indexOf(num)
+                indexArray.splice(z,1)
+                }
+            })
+            let indexArraySlice = indexArray.slice(-2)
+            console.log(indexArray)
+            console.log(`from2:${from2}, index: ${index}, indexArraySlice: ${indexArraySlice}`)
+            console.log(index == indexArraySlice[0])
+            console.log(expx)
             if(isNaN(exp3) == false){
-                let exp4 = valuesArray[0] - exp3
-                valuesArray.splice(0,1, exp4)
-                console.log(valuesArray)
+                if(index == indexArraySlice[0]){
+                    valuesArray.pop()
+                    indexArray.pop()
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 - exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                }else if (index != indexArraySlice[0]){
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 - exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                }
+                
             }else if(isNaN(exp3) == true){
                 console.log("exp3 is NaN")
             }
             console.log(valuesArray)
+        
         }
-        if(exp1.includes("×")){
+        if(exp1.includes("×") == true){
+            //let indexArray = []
             let exp2 = value.split("×")
             let from = Array.from(exp2)
             let from2 = from.pop()
             let exp3 = from2 * 1
+            let index = expx.indexOf(from2, -1)
+            indexArray.push(index)
+            indexArray.forEach(num => {
+                if(num == -1) {
+                let z = indexArray.indexOf(num)
+                indexArray.splice(z,1)
+                }
+            })
+            let indexArraySlice = indexArray.slice(-2)
+            console.log(indexArray)
             if(isNaN(exp3) == false){
-                let exp4 = valuesArray[0] * exp3
-                valuesArray.splice(0,1, exp4)
-                console.log(valuesArray)
+                if(index == indexArraySlice[0]){
+                    valuesArray.pop()
+                    indexArray.pop()
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 * exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                }else if (index != indexArraySlice[0]){
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 * exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                }
+                
             }else if(isNaN(exp3) == true){
                 console.log("exp3 is NaN")
             }
             console.log(valuesArray)
+        
         }
-        if(exp1.includes("÷")){
+        if(exp1.includes("÷") == true){
+            //let indexArray = []
+            let exp2 = value.split("÷")
+            let from = Array.from(exp2)
+            let from2 = from.pop()
+            let exp3 = from2 * 1
+            let index = expx.indexOf(from2, -1)
+            indexArray.push(index)
+            indexArray.forEach(num => {
+                if(num == -1) {
+                let z = indexArray.indexOf(num)
+                indexArray.splice(z,1)
+                }
+            })
+            let indexArraySlice = indexArray.slice(-2)
+            console.log(indexArray)
+            if(isNaN(exp3) == false){
+                if(index == indexArraySlice[0]){
+                    valuesArray.pop()
+                    indexArray.pop()
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 / exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                }else if (index != indexArraySlice[0]){
+                    let slice1 = valuesArray.slice(-1) * 1
+                    let exp4 = slice1 / exp3
+                    valuesArray.push(exp4)
+                    console.log(valuesArray)
+                }
+                
+            }else if(isNaN(exp3) == true){
+                console.log("exp3 is NaN")
+            }
+            console.log(valuesArray)
+        
+        }
+        /*if(exp1.includes("÷")){
             let exp2 = value.split("÷")
             let from = Array.from(exp2)
             /*let filterFrom = from.filter(num => num.includes("%"))
@@ -95,7 +216,7 @@ numbers.forEach(number => number.addEventListener("click",() => {
             let foo1 = foo / 100
             from.splice(0,1, foo1)
             console.log(foo1)
-            console.log(from)*/
+            console.log(from)
             
             let from2 = from.pop()
             let exp3 = from2 * 1
@@ -106,16 +227,26 @@ numbers.forEach(number => number.addEventListener("click",() => {
             }else if(isNaN(exp3) == true){
                 console.log("exp3 is NaN")
             }
-        }else if(value.length >= 0 && exp1.includes("+","−","×","÷") == false){
+        }*/else if(value.length >= 0 && exp1.includes("+","−","×","÷") == false){
             let value1 = value * 1
+            let index = expx.indexOf(value)
+            let indexArraySlice = indexArray.slice(-1)
+            indexArray.push(index)
             if(isNaN(value1) == false){
-                valuesArray.splice(0,1, value1)
-                console.log(value1)
+                if(index == indexArraySlice[0]){
+                    indexArray.pop()
+                    valuesArray.splice(0,1, value1)
+                }else if(index != indexArraySlice){
+                    valuesArray.splice(0,1, value1)
+                }
+                //valuesArray.splice(0,1, value1)  
+                console.log(valuesArray)   
+                console.log(indexArray) 
             }else if(isNaN(value1) == true){
                 console.log("value1 is NaN")
             }
            
-        } 
+        }
     },500)
        
     })
@@ -188,23 +319,38 @@ squared.addEventListener("click", () =>{
 ac.addEventListener("click", () => {
     expression.setAttribute("value", "")
     result.setAttribute("value", "")
+    valuesArray = []
+    indexArray = []
 })
 del.addEventListener("click", () => {
     let exp = []
     let x = expression.value
     let x1 = x.toString()
     let x2 = x1.split("")
-    x2.pop()
+    let x5 = x2.pop()
     let x3 = x2.join("")
-    console.log(x3)
+    console.log(x5)
     expression.setAttribute("value", x3) 
-    result.setAttribute("value", "")  
+    result.setAttribute("value", "") 
+    if (x5 !== "+" && x5 !== "−"  && x5 !== "×" && x5 !== "÷") {
+        valuesArray.pop()
+        console.log(valuesArray)
+        console.log(x5 !== "−")
+        result.setAttribute("value", valuesArray.slice(-1))
+    }else if(x5 === "+" && x5 === "−" && x5 === "×" && x5 === "÷") {
+        console.log(x5)
+    }
+        
+     
 })
 equals.addEventListener("click", results)
-function results() {
-    result.setAttribute("value", valuesArray[0])
-    //let expArray = []
-    //let exp = expression.value
+function results() { 
+    let lastValue = valuesArray.slice(-1)
+    console.log(lastValue[0])
+    result.setAttribute("value", lastValue[0])
+    console.log(result.value)
+    let expArray = []
+    let exp = expression.value
     /*let w = exp.split("+")
     let z = Array.from(w)
     let checkPercentage = z.filter(num => num.includes("%"))
@@ -217,36 +363,49 @@ function results() {
         })
     }*/
        
-    /*let exp4 = exp.split("")
+    let exp4 = exp.split("")
     if(exp4.includes("+")){
         let exp2 = exp.split("+")
         let y = Array.from(exp2)
         let update = y.map(age => age * 1)
         let results = update.reduce((pre, cur) => pre + cur)
-        result.setAttribute("value", results)
+        if(isNaN(results) == true){
+            console.log("NaN results returned")
+        }else if(isNaN(results) == false){
+            result.setAttribute("value", results)
+        }
 
     }else if(exp4.includes("−")){
         let exp2 = exp.split("−")
         let y = Array.from(exp2)
         let update = y.map(age => age * 1)
         let results = update.reduce((pre, cur) => pre - cur)
-        result.setAttribute("value", results)
-        console.log(results)
+        if(isNaN(results) == true){
+            console.log("NaN results returned")
+        }else if(isNaN(results) == false){
+            result.setAttribute("value", results)
+        }
     }else if(exp4.includes("×")){
         let exp2 = exp.split("×")
         let y = Array.from(exp2)
         let update = y.map(age => age * 1)
         let results = update.reduce((pre, cur) => pre * cur)
-        result.setAttribute("value", results)
-        console.log(results)
+        if(isNaN(results) == true){
+            console.log("NaN results returned")
+        }else if(isNaN(results) == false){
+            result.setAttribute("value", results)
+        }
     }else if(exp4.includes("÷")){
         let exp2 = exp.split("÷")
         let y = Array.from(exp2)
         let update = y.map(age => age * 1)
         let results = update.reduce((pre, cur) => pre / cur)
-        result.setAttribute("value", results)
-        console.log(results)
-    }else if(exp4.includes("%")){
+        if(isNaN(results) == true){
+            console.log("NaN results returned")
+        }else if(isNaN(results) == false){
+            result.setAttribute("value", results)
+        }
+    }/*else if(exp4.includes("%")){
         console.log(exp4)
         /*let y = Array.from(exp2)
         let update = y.map(age => age * 1)
